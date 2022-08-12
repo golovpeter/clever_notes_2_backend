b46aa5c580fedb86e749416709ff74f31640fff6
Голов Петр <petya-golov@mail.ru>
2022-08-12 12:46:29 +0300

    added parse jwt token fuctions

--- internal/common/token_generator/token_generator.go
+++ internal/common/token_generator/token_generator.go
@@ -1,6 +1,7 @@
 package token_generator
 
 import (
+	"fmt"
 	"github.com/golang-jwt/jwt/v4"
 	"os"
 	"time"
@@ -11,7 +12,10 @@ type tokenClaims struct {
 	Username string
 }
 
-const tokenTTL = time.Hour
+const (
+	tokenTTL        = time.Hour
+	refreshTokenTTL = time.Hour * 720
+)
 
 func GenerateJWT(username string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
@@ -27,8 +31,32 @@ func GenerateJWT(username string) (string, error) {
 
 func GenerateRefreshJWT() (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
-		ExpiresAt: &jwt.NumericDate{Time: time.Now().Add(time.Hour * 720)},
+		ExpiresAt: &jwt.NumericDate{Time: time.Now().Add(refreshTokenTTL)},
 	})
 
 	return token.SignedString([]byte(os.Getenv("SIGNINKEY")))
+}
+
+func ParseToken(inputToken string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(inputToken, func(token *jwt.Token) (i interface{}, err error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %s", token.Header["alg"])
+		}
+
+		return []byte(os.Getenv("SIGNINKEY")), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+
+	if !ok {
+		_ = fmt.Errorf("error get user claims from token")
+		return nil, err
+	}
+
+	return claims, nil
+
 }
